4b8af55b9fd6b0ce04c6eccd3296d135357549e1
Jannis Frank <frank.jannis@gmail.com>
2025-07-04 12:44:32 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+
+struct CloneConfig {
+    url: String,
+    target: PathBuf,
+}
+
+struct InstallConfig {
+    name: String,
+}
+
+fn main() {
+    println!("Starting Profile Setup");
+
+    
+}
