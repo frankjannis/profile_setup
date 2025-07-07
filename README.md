7a3deb901a5fdebc51996217860f7c6ac208d9b8
Jannis Frank <frank.jannis@gmail.com>
2025-07-07 13:35:36 +0200

    Samba setup

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 sudo pacman -S git rust --needed
 git clone https://github.com/frankjannis/profile_setup.git ~/Documents/profile_setup
 cd ~/Documents/profile_setup
-cargo run
+sudo cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::{io::Write, path::Path};
 fn main() {
     println!("Starting Profile Setup");
 
-    let home = std::env::var("HOME").unwrap();
-    let home = Path::new(&home);
+    // let home = std::env::var("HOME").unwrap();
+    // let home = Path::new(&home);
+    let home = Path::new("/home/jannis");
     println!("Home at: {home:?}");
     ask_confirmation();
 
@@ -14,8 +15,7 @@ fn main() {
         .output().unwrap();
     if !output.status.success() {
         println!("Installing yay");
-        let output = std::process::Command::new("sudo")
-            .arg("pacman")
+        let output = std::process::Command::new("pacman")
             .arg("-S")
             .arg("yay")
             .status().unwrap();
@@ -73,16 +73,53 @@ fn main() {
             .status().unwrap();
         handle_exit_status(output);
     }
+
+
+    println!("Setting up samba shares");
+    ask_confirmation();
+    let samba_credentials_path = std::path::Path::new("/etc/samba-credentials");
+    let mut samba_credentials = std::fs::File::create(samba_credentials_path).unwrap();
+    writeln!(samba_credentials, "username=jannis\npassword=").unwrap();
+    let output = std::process::Command::new("helix")
+        .arg(samba_credentials_path)
+        .status().unwrap();
+    handle_exit_status(output);
+    let output = std::process::Command::new("chmod")
+        .arg("600")
+        .arg(samba_credentials_path)
+        .status().unwrap();
+    handle_exit_status(output);
+    let fstab_path = std::path::Path::new("/etc/fstab");
+    let mut fstab = std::fs::OpenOptions::new()
+        .append(true).open(fstab_path).unwrap();
+    for share in [
+        "video",
+        "dropboxReadOnly",
+    ] {
+        writeln!(fstab, "//172.16.97.200/{share} /mnt/truenas/{share} cifs credentials={},iocharset=utf8,uid=1000,gid=1000,file_mode=0777,dir_mode=0777,nofail 0 0", samba_credentials_path.display()).unwrap();
+    };
+    let output = std::process::Command::new("helix")
+        .arg(fstab_path)
+        .status().unwrap();
+    handle_exit_status(output);
+    let output = std::process::Command::new("systemctl")
+        .arg("daemon-reload")
+        .status().unwrap();
+    handle_exit_status(output);
+    let output = std::process::Command::new("mount")
+        .arg("-a")
+        .status().unwrap();
+    handle_exit_status(output);
 }
 
 fn ask_confirmation() {
-    print!("Continue? [y/N]: ");
+    print!("Continue? [Y/n]: ");
     std::io::stdout().flush().unwrap();
 
     let mut input = String::new();
     if let Ok(_) = std::io::stdin().read_line(&mut input) {
         let input = input.trim().to_lowercase();
-        if input == "y" || input == "yes" {
+        if input == "y" || input == "yes" || input == "" {
             return;
         }
     }
